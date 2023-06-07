e6359b9e362de9c419ae16a8ce17850499450d7d
luyang93 <2562986-luyang93@users.noreply.gitlab.com>
2023-06-08 00:04:53 +0800

    Builder example6.
    Signed-off-by: luyang93 <2562986-luyang93@users.noreply.gitlab.com>

---
+++ Builder/example6/iBuilder.go
@@ -0,0 +1,18 @@
+package main
+
+type iBuilder interface {
+	setWindowType()
+	setDoorType()
+	setNumberFloor()
+	getHouse() house
+}
+
+func getBuilder(builderType string) iBuilder {
+	if builderType == "normal" {
+		return &normalBuilder{}
+	}
+	if builderType == "igloo" {
+		return &iglooBuilder{}
+	}
+	return nil
+}
